16b9dc96f94d183bffdf42d721ab2e20693eb164
Zaheen8088 <134678431+Zaheen8088@users.noreply.github.com>
2023-05-26 12:16:09 +0530

    Add files via upload

---
+++ matplotlib.py
@@ -0,0 +1,32 @@
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TKRgg')
+def selection_sort(seq):
+    n=len(seq)
+    for i in range(n):
+        min=i
+        for j in range(i+1,n):
+            if seq[i]<seq[min]:
+                temp=seq[min]
+                seq[min]=seq[j]
+                seq[j]=temp
+seq=[10,70,15,8,80,90,20]
+
+print("Before Sorting",seq)
+selection_sort(seq)
+print("after sorting",seq)
+X=list(range(1.10000))
+plt.plot(z,[Yy for y in x])
+plt.xlabel("Selection_Sort_Time_complexty is o(n)400b2)")
+plt.xlabel("input")
+plt.ylabel("Time")
+plt.show()
+
+      
+
+
+
+
+            
+            
+            
